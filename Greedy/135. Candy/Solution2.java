42422e50e57770bb048e5d5b18fb6b33191fb81b
Xudong ZHAO <xudong.zhao@protonmail.com>
2022-09-13 03:39:40 +0200

    135. Candy 两个算法

--- Greedy/135. Candy/Solution2.java
+++ Greedy/135. Candy/Solution2.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution2 {
     public int candy(int[] ratings) {
         int size = ratings.length;
         if (size < 2) {
@@ -8,13 +8,13 @@ class Solution {
         Arrays.fill(c, 1);
         for (int i = 1; i < size; ++i) {
             if (ratings[i] > ratings[i - 1]) {
-                c[i] = c[i-1] + 1;
+                c[i] = c[i - 1] + 1;
             }
         }
 
         for (int i = size - 1; i > 0; --i) {
-            if (ratings[i] < ratings[i - 1]&& c[i - 1]<= c[i]) {
-                c[i - 1] = c[i] + 1;
+            if (ratings[i] < ratings[i - 1]) {
+                c[i - 1] = Math.max(c[i - 1], c[i] + 1);
             }
         }
 
